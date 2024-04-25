156101d1866368b0d07cb4affcc59e494fa8437e
Kévin Gst <kevin.gosset25@gmail.com>
2024-04-25 09:21:39 -0400

    fix head

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Eboos</title>
+      <title>Eboos NFT</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Ce sera probablement ton premier NFT mais certainement pas ton dernier..." />
       <link rel="icon" href="/favicon.ico" />
