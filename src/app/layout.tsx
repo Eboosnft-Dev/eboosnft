9b63926e4d2e5842ad4912d738cf65d71267e937
Kévin Gst <kevin.gosset25@gmail.com>
2024-04-25 13:01:20 -0400

    fix
    bg gray

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="bg-black font-nunito">{children}</body>
+      <body className="bg-slate-800 font-nunito">{children}</body>
     </html>
   );
 }
