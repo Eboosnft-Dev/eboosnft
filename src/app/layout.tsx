4ec41055a2df8614448572cf6ee9ed5bb30e4204
Kévin Gst <kevin.gosset25@gmail.com>
2024-05-11 14:46:23 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
+'use client';
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({
   children,
